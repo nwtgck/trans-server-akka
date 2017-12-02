c7b44083bcf9a00b5ff645916d3a13b4a1a09e78
Ryo Ota <nwtgck@gmail.com>
2017-12-02 20:26:50 +0900

    [change] Change /help message shorten

--- src/main/scala/io/github/nwtgck/trans_server/Core.scala
+++ src/main/scala/io/github/nwtgck/trans_server/Core.scala
@@ -162,52 +162,39 @@ class Core(db: Database, fileDbPath: String){
       } ~
       path(Setting.GetRouteName.Help) {
         complete( // TODO Should(?) move text content somewhere
-          s"""
-             |Help for trans (${BuildInfo.version})
-             |(Repository: https://github.com/nwtgck/trans-server-akka)
-             |
-             |(NOTE: hogehoge.io is a dummy domain)
-             |
-             |====== wget =====
-             |# Send
-             |wget -q -O - https://hogehoge.io --post-file=test.txt
-             |
-             |# Get
-             |wget https://hogehoge.io/a3h
-             |('a3h' is File ID of test.txt)
-             |
-             |# Delete
-             |wget --method=DELETE https://hogehoge.io/a3h
-             |
-             |===== curl ======
-             |# Send
-             |curl https://hogehoge.io --data-binary @test.txt
-             |
-             |# Get
-             |curl https://hogehoge.io/a3h > mytest.txt
-             |
-             |# Delete
-             |curl -X DELETE https://hogehoge.io/a3h
-             |
-             |===== Option Example =====
-             |
-             |# Send (duration: 30 sec, Download limit: once, ID Length: 16, Delete key: 'mykey1234')
-             |wget -q -O - 'https://hogehoge.io/?duration=30s&get-times=1&id-length=16&delete-key=mykey1234' --post-file=./test.txt
-             |
-             |# Delete with delete key
-             |wget --method DELETE 'https://hogehoge.io/a3h?delete-key=mykey1234'
-             |
-             |===== Installation of trans-cli =====
-             |pip3 install --upgrade git+https://github.com/nwtgck/trans-cli-python@master
-             |
-             |
-             |
-             |===== Tip: directory sending (zip) =====
-             |zip -q -r - ./mydir | curl -X POST https://hogehoge.io --data-binary @-
-             |
-             |===== Tip: directory sending (tar.gz) =====
-             |tar zfcp - test/ | curl -X POST https://hogehoge.io --data-binary @-
-             |""".stripMargin
+          s"""|Help for trans (${BuildInfo.version})
+              |(Repository: https://github.com/nwtgck/trans-server-akka)
+              |(NOTE: hogehoge.io is a dummy domain)
+              |
+              |====== wget =====
+              |# Send  : wget -q -O - https://hogehoge.io --post-file=test.txt
+              |# Get   : wget https://hogehoge.io/a3h
+              |# Delete: wget --method=DELETE https://hogehoge.io/a3h
+              |('a3h' is File ID of test.txt)
+              |
+              |===== curl ======
+              |# Send  : curl https://hogehoge.io --data-binary @test.txt
+              |# Get   : curl https://hogehoge.io/a3h > mytest.txt
+              |# Delete: curl -X DELETE https://hogehoge.io/a3h
+              |('a3h' is File ID of test.txt)
+              |
+              |===== Option Example =====
+              |# Send (duration: 30 sec, Download limit: once, ID length: 16, Delete key: 'mykey1234')
+              |wget -q -O - 'https://hogehoge.io/?duration=30s&get-times=1&id-length=16&delete-key=mykey1234' --post-file=./test.txt
+              |
+              |# Delete with delete key
+              |wget --method DELETE 'https://hogehoge.io/a3h?delete-key=mykey1234'
+              |
+              |
+              |------ Installation of trans-cli ------
+              |pip3 install --upgrade git+https://github.com/nwtgck/trans-cli-python@master
+              |
+              |------ Tip: directory sending (zip) ------
+              |zip -q -r - ./mydir | curl -X POST https://hogehoge.io --data-binary @-
+              |
+              |------ Tip: directory sending (tar.gz) ------
+              |tar zfcp - ./mydir/ | curl -X POST https://hogehoge.io --data-binary @-
+              |""".stripMargin
         )
       } ~
       path(Remaining) { fileIdStr =>
