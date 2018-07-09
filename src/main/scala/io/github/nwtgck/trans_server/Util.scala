285672f17fcdc312d9ac1f0d47fe727369cd6295
Ryo Ota <nwtgck@gmail.com>
2018-07-09 23:32:40 +0900

    [refactor/test] Add test for `Util.generateHttpsConnectionContext()`

--- src/main/scala/io/github/nwtgck/trans_server/Util.scala
+++ src/main/scala/io/github/nwtgck/trans_server/Util.scala
@@ -1,5 +1,11 @@
 package io.github.nwtgck.trans_server
 
+import java.io.{FileInputStream, InputStream}
+import java.security.{KeyStore, SecureRandom}
+
+import akka.http.scaladsl.{ConnectionContext, HttpsConnectionContext}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
+
 object Util {
   // Generate hashed key
   // (from: http://www.casleyconsulting.co.jp/blog-engineer/java/%E3%80%90java-se-8%E9%99%90%E5%AE%9A%E3%80%91%E5%AE%89%E5%85%A8%E3%81%AA%E3%83%91%E3%82%B9%E3%83%AF%E3%83%BC%E3%83%89%E3%82%92%E7%94%9F%E6%88%90%E3%81%99%E3%82%8B%E6%96%B9%E6%B3%95/)
@@ -29,5 +35,35 @@ object Util {
       sb.append("%02x".format(b & 0xff))
     }
     sb.toString()
+  }
+
+  /**
+    * Generate a HttpsConnectionContext
+    *
+    * hint from: http://doc.akka.io/docs/akka-http/current/scala/http/server-side-https-support.html
+    * @return
+    */
+  def generateHttpsConnectionContext(keystorePassword: String, keyStorePath: String): HttpsConnectionContext = {
+
+    // Manual HTTPS configuration
+    val password: Array[Char] = keystorePassword.toCharArray // do not store passwords in code, read them from somewhere safe!
+
+    val ks: KeyStore = KeyStore.getInstance("jks")
+    val keystore: InputStream = new FileInputStream(keyStorePath)
+
+    require(keystore != null, "Keystore required!")
+    ks.load(keystore, password)
+
+    val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(ks, password)
+
+    val tmf: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+    tmf.init(ks)
+
+    val sslContext: SSLContext = SSLContext.getInstance("TLS")
+    sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom)
+    val httpsConnectionContext: HttpsConnectionContext = ConnectionContext.https(sslContext)
+
+    httpsConnectionContext
   }
 }
